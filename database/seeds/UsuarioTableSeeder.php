52264e4bcc575fcc61220736b6aa2bc479ade3ec
Higor Silva <higor.junio@hotmail.com>
2019-05-07 20:28:10 -0300

    criado seed de usuario

---
+++ database/seeds/UsuarioTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsuarioTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $usuario = App\User::create(['id' => '5','CPF' => '12345678910','email' => 'geminusueg@gmail.com','password' => '$2y$10$DRqJCYgwhnaJ3JjLhDhN2Or4kU7kRtS7w3aLlwfIQwHcNjt7mYcw.','created_at' => '2019-02-22 13:49:44','updated_at' => '2019-03-17 13:39:55','_token' => 'ighDQ8ACl2UBbG6NkRJIS6uGUr70Xb6ad5EqXgtZ']);
+        $usuario = App\User::create(['id' => '6','CPF' => '12345678918','email' => 'q@gmail.com','password' => '$2y$10$BWwsQHcghdNz7uBBtIveAOJrvYx5lV7ej1ycuyPtGr5iqXEpmP9TO','created_at' => '2019-02-23 01:10:14','updated_at' => '2019-03-26 23:21:45','_token' => 'U81igFZH4wd0cANYjup8RaFmqt1gbnBuYDeWraQX']);
+        $usuario = App\User::create(['id' => '15','CPF' => '07728366041','email' => 'ap@gmail.com','password' => '$2y$10$hSndeudMcWqPV/D8O/RLMOVsrQRGzdEUBvEFJ5wqzI8IPZTfkEUfa','created_at' => '2019-02-23 12:44:22','updated_at' => '2019-03-15 20:27:13','_token' => 'bxPfbq8l0Tb8LL35ww4SnXuVCeb7WOsVTrWE2ne5']);
+        $usuario = App\User::create(['id' => '17','CPF' => '10283501431','email' => 'resp@gmail.com','password' => '$2y$10$1i3wRAeejfkB.2IL5nWJ5u78o..yibthriy99JRj892byf3XxRJCe','created_at' => '2019-02-27 13:04:05','updated_at' => '2019-02-27 13:04:05','_token' => NULL]);
+        $usuario = App\User::create(['id' => '41','CPF' => '56598842036','email' => 'dyost@yahoo.com','password' => '$2y$10$f9y3U07GLboLLz30FuOeIO8f8cWyl2soFAyJ03ZCj8a.TtCa9HgVG','created_at' => '2019-03-01 12:51:53','updated_at' => '2019-03-01 12:51:53','_token' => NULL]);
+        $usuario = App\User::create(['id' => '42','CPF' => '77906556358','email' => 'alvera12@muller.com','password' => '$2y$10$UarEzYI4YuIqY7KODwoMdOrzhPxqsRK0Vrf2d73BLvPXxwXWgdWnK','created_at' => '2019-03-01 12:51:53','updated_at' => '2019-03-01 12:51:53','_token' => NULL]);
+        $usuario = App\User::create(['id' => '44','CPF' => '70278382345','email' => 'beier.aric@gmail.com','password' => '$2y$10$lWClJuBWL/we5kW24FhwfuE2r9Lh.GIYw4vJM.JdGXOcYuLghXZtS','created_at' => '2019-03-01 12:51:53','updated_at' => '2019-03-01 12:51:53','_token' => NULL]);
+        $usuario = App\User::create(['id' => '45','CPF' => '91550947087','email' => 'shea.monahan@runte.com','password' => '$2y$10$ZiJYHowdolIwroIh/jZlBOC4Nun8thzUJpjPaSxyS0Oc0MJ.RrNLO','created_at' => '2019-03-01 12:51:53','updated_at' => '2019-04-25 18:45:12','_token' => 'LBGFZl6WAwQl7OStiITwk25GlCBVj1hKQztGS0rE']);
+        $usuario = App\User::create(['id' => '46','CPF' => '88670941946','email' => 'wfarrell@herzog.info','password' => '$2y$10$PvC9ZDBZv2UvZp.EQl4udeFe3e7l7pvc09SGo0N99c89SGvlGfckK','created_at' => '2019-03-01 12:51:53','updated_at' => '2019-03-01 12:51:53','_token' => NULL]);
+        $usuario = App\User::create(['id' => '47','CPF' => '62353397393','email' => 'jaiden.koepp@gmail.com','password' => '$2y$10$tBjP2Lftlk7YJvKVhx2kjOb19U44ICkCllZbuUGCFQD1qDR5/I526','created_at' => '2019-03-01 12:51:53','updated_at' => '2019-03-01 12:51:53','_token' => NULL]);
+        $usuario = App\User::create(['id' => '48','CPF' => '58852805317','email' => 'krista.wintheiser@skiles.biz','password' => '$2y$10$2mxI8M9wgKD2i7FQJUQ/7uPKFiudZVRMJiYLjc.JbP07IR23nU7Oe','created_at' => '2019-03-01 12:51:53','updated_at' => '2019-03-01 12:51:53','_token' => NULL]);
+        $usuario = App\User::create(['id' => '50','CPF' => '40212268351','email' => 'lwalsh@yahoo.com','password' => '$2y$10$Tkc0kFIbJxwgvEshasWeAOuU3PFEuzT8gWeedp9WfPdXURlbRdHDC','created_at' => '2019-03-01 12:51:54','updated_at' => '2019-03-01 12:51:54','_token' => NULL]);
+        $usuario = App\User::create(['id' => '51','CPF' => '70330589610','email' => 'mitchel41@gmail.com','password' => '$2y$10$xB66WjAPq605gFLkeZGFfem45CIuLExwWkCW/IlqeC1lBSB8flIMK','created_at' => '2019-03-01 12:51:54','updated_at' => '2019-03-01 12:51:54','_token' => NULL]);
+        $usuario = App\User::create(['id' => '52','CPF' => '48429078097','email' => 'christiansen.pattie@sawayn.com','password' => '$2y$10$U0Trp2A8epWqdACodOlMkuRhsdbV2dytdcW2p4NFcsmsjPqXVbkT6','created_at' => '2019-03-01 12:51:54','updated_at' => '2019-03-01 12:51:54','_token' => NULL]);
+        $usuario = App\User::create(['id' => '53','CPF' => '48956998489','email' => 'xander17@borer.com','password' => '$2y$10$oGtx0qBmONmYCosS9HuTA.Jm..AfoQgEwzRi8Es47Jmi0c9K0.O.O','created_at' => '2019-03-01 12:51:54','updated_at' => '2019-03-01 12:51:54','_token' => NULL]);
+        $usuario = App\User::create(['id' => '56','CPF' => '47757510698','email' => 'maynard.mohr@yahoo.com','password' => '$2y$10$Kb5BVGuZogTwIp/Iqe/bAel72k9UuNToWCqVwFpXZhuuhAWhlhytK','created_at' => '2019-03-01 12:51:54','updated_at' => '2019-03-01 12:51:54','_token' => NULL]);
+        $usuario = App\User::create(['id' => '58','CPF' => '34324448608','email' => 'king.noemie@yahoo.com','password' => '$2y$10$5wpUSoQHtMSUqrlSOwMSXunW3YVEmnACyP2lwpLMsVS9YVhtGDir.','created_at' => '2019-03-01 12:51:54','updated_at' => '2019-03-01 12:51:54','_token' => NULL]);
+        $usuario = App\User::create(['id' => '59','CPF' => '77575870101','email' => 'claudia89@hotmail.com','password' => '$2y$10$i0pkgP9.M14qX2F6bOlleOXydlmHhZEM1QiBZwJ8SBm5CoUevs4Aa','created_at' => '2019-03-01 12:51:54','updated_at' => '2019-03-01 12:51:54','_token' => NULL]);
+        $usuario = App\User::create(['id' => '66','CPF' => '12345678914','email' => 'emailmail@gmail.com','password' => '$2y$10$5T9mDXY616Lpl.gnvL/lluGW1YijYbxG0lDtFv/auKtmF.lxL73qu','created_at' => '2019-03-16 15:06:01','updated_at' => '2019-03-16 15:06:01','_token' => NULL]);
+        $usuario = App\User::create(['id' => '67','CPF' => '15789654615','email' => 'silverbolt@gmail.com','password' => '$2y$10$uQ/CHNQCCkWrjY2vS8qWZ.KIHR9nvEc32Qtph0NKE.rzdCN9UYfMe','created_at' => '2019-03-16 15:09:28','updated_at' => '2019-03-17 13:06:14','_token' => 'J3BGkCT0nmpC70eMkA0f3KYZVa9pKNaqZCsslflg']);
+        $usuario = App\User::create(['id' => '68','CPF' => '36589541201','email' => 'mail@gmail.com','password' => '$2y$10$lMKw7kqM5NW8b/UqTgLWqe.UiRkvVGWz40slqLOcHM3Pe1RfwIclW','created_at' => '2019-03-16 15:28:18','updated_at' => '2019-03-16 15:28:18','_token' => NULL]);
+        $usuario = App\User::create(['id' => '69','CPF' => '45876541212','email' => 'bb@gmail.com','password' => '$2y$10$kzzmILYfAIDS58zFUKsW9eZWse17zaHB2u.0LsrSRleA2SZozLyaC','created_at' => '2019-03-16 15:29:46','updated_at' => '2019-03-16 15:29:46','_token' => NULL]);
+        $usuario = App\User::create(['id' => '73','CPF' => '58964785412','email' => 'mailoi@gmail.com','password' => '$2y$10$q3uvp4eAg/PEce9aOC0BuObVsvUaZBGUD0oxZu7Ny3rJ95.xv/LZS','created_at' => '2019-03-17 12:55:45','updated_at' => '2019-03-17 12:55:45','_token' => NULL]);
+        $usuario = App\User::create(['id' => '74','CPF' => '12345678911','email' => 'emailooo@gmail.com','password' => '$2y$10$61lZzvP9Sdhqw9vgQl.1xuTAPfps6r5FOGoVc5JoyV71HP6nTkqH6','created_at' => '2019-03-25 13:02:26','updated_at' => '2019-03-26 23:20:50','_token' => 'U81igFZH4wd0cANYjup8RaFmqt1gbnBuYDeWraQX']);
+        $usuario = App\User::create(['id' => '75','CPF' => '30299051013','email' => 'emailbemgrande@gmail.com','password' => '$2y$10$oW0zBNMzQJo0ci1KAhvK.eXvSn9N01IGBtBaToe3Wr97XDhsez2e2','created_at' => '2019-05-03 18:38:33','updated_at' => '2019-05-03 18:38:58','_token' => 'hYG8ZegKpHSZOAMp4FUxCCo7J6lEpZkUF24rxuTc']);
+    }
+}
